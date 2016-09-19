d811495371fc3e5fec26b14fdafa4485ffde8be5
Pedro Lima <pedro.nym@gmail.com>
2016-09-19 17:47:10 +0100

    First commit with base files setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="todo-main">
+    </div>
+</template>
+
+<script>
+    import TodoCreate from './components/TodoCreate'
+
+    export default {
+        components: {
+            TodoCreate
+        }
+    }
+</script>
---
+++ src/components/TodoCreate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="todo-create">
+        <input type="text" placeholder="{{ placeholderText }}">
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            placeholderText: 'Create a Todo!'
+        }
+    }
+}
+</script>
