6caccd4742f408a728f9114710b0882a6a6b948d
Pedro Lima <plima@parceiro.sonae.pt>
2016-10-06 08:16:40 +0100

    Creating and deleting todos is done. Going to try and work out editing later

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,72 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+
+    <todo-list v-on:add-todo="addTodo"></todo-list>
+
+    <section class="todo-wrapper">
+      <todo-list-item v-for="todo in todos" :uid="todo.id" :title="todo.title" :completed="todo.completed" v-on:remove-todo="removeTodo" v-on:complete-todo="completeTodo">
+
+      </todo-list-item>
+    </section>
+    
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Storage from './storage.js'
+import TodoList from './components/TodoList'
+import TodoListItem from './components/TodoListItem'
+
+var filters = {
+  active: function (todos) {
+    return todos.filter(function (todo) {
+      return !todo.completed
+    })
+  }
+}
 
 export default {
   components: {
-    Hello
+    TodoList,
+    TodoListItem
+  },
+  data: function () {
+    return {
+      todos: Storage.fetch()
+    }
+  },
+  methods: {
+    addTodo (todoText) {
+      this.todos.push({
+        id: Storage.uid++,
+        title: todoText,
+        completed: false
+      })
+    },
+    removeTodo (todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    },
+    completeTodo (todo) {
+      // this.todos[todoId].completed = true
+      // this.todos = filters.active(this.todos)
+    }
+  },
+  watch: {
+    todos: {
+      handler: function (todos) {
+        Storage.save(todos)
+      },
+      deep: true
+    }
+  },
+  filters: {
+    removeCompleted: function () {
+      this.todos = filters.active(this.todos)
+    }
   }
 }
 </script>
 
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+<style lang="scss">
+@import 'assets/scss/app';
 </style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="todo-list-item" v-bind:class="{ done: completed }">
+    
+    <div class="todo-list-item__text" >
+        <h1 class="todo-list-item__title">{{title}}</h1>
+        <small class="todo-list-item__date"><span class="entypo-calendar"></span> Created on {{date}}</small>
+    </div>
+
+    <div class="todo-list-item__actions">
+        <a class="entypo-check" @click="completeTodo"></a>
+        <a class="entypo-pencil" @click="editTodo"></a>
+        <a class="entypo-trash" @click="deleteTodo"></a>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: new Date()
+    }
+  },
+  methods: {
+    deleteTodo () {
+      this.$emit('remove-todo', this)
+    },
+    editTodo () {
+      this.$emit('edit-todo', this)
+    },
+    completeTodo () {
+      this.$emit('complete-todo', this)
+    }
+  },
+  props: ['uid', 'title', 'completed']
+}
+</script>
